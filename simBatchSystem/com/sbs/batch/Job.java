bb82a342d8f64ac1a063c1c8eeb18956efdee9c9
huangdada <huangdada@huangdada-PC>
2014-07-16 20:56:28 +0900

    Signed-off-by: huangdada <huangdada@huangdada-PC>

--- simBatchSystem/com/sbs/batch/Job.java
+++ simBatchSystem/com/sbs/batch/Job.java
@@ -1,11 +1,16 @@
 package com.sbs.batch;
 
+import javax.swing.JTable;
+
 public class Job {
 	private int time;
 	private String stat;
+	private String id;
+	private static int currI=0;
 	public Job(int t){
-		setStat("waiting");
+		stat="waiting";
 		setTime(t);
+		setId();
 	}
 	public int getTime() {
 		return time;
@@ -15,10 +20,27 @@ public class Job {
 	}
 	public void setStat(String string) {
 		// TODO Auto-generated method stub
+		JTable jt = Sbs.mf.getTable();
+		int row = 0;
+		for(int i=0;i<jt.getRowCount();i++){
+			if(jt.getValueAt(i, 0).toString().equalsIgnoreCase(id)){
+				row = i;
+				break;
+			}
+		}
+		
+		jt.setValueAt(string, row, 1);
+		
 		stat=string;
 	}
 	public String getStat(){
 		return stat;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId() {
+		this.id = currI+++"";
 	}
 
 }
--- simBatchSystem/com/sbs/batch/Sbs.java
+++ simBatchSystem/com/sbs/batch/Sbs.java
@@ -2,15 +2,29 @@ package com.sbs.batch;
 
 import java.util.ArrayList;
 
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.sbs.gui.MyFrame;
+
 
 public class Sbs {
 	static{
 		pool = new ArrayList<Slot>();
 		queue = new ArrayList<Job>();
+		mf = new MyFrame();
 	}
-	public static boolean addSlot(String vm){
-		Slot slot = new Slot(vm);
-		return pool.add(slot);
+	public static boolean addSlot(String num){
+		int n = Integer.parseInt(num);
+		for(int i=0;i<n;i++){
+			String vm = "cloud1,v00"+i+",ip00"+i;
+			Slot slot = new Slot(vm);
+			if(!pool.add(slot)){
+				return false;
+			}
+		}
+
+		return true;
 	}
 	
 	public static boolean rmSlot(String name){
@@ -31,36 +45,93 @@ public class Sbs {
 			t.start();  
 	}
 	public static void execute(){
+
 		while(true){
-			if(queue.isEmpty()||pool.isEmpty())continue;
-			for(Job job:queue){
-				
-				if(job.getStat().equalsIgnoreCase("waiting")){
-					for(Slot slot : pool){
-						if(!slot.isActivity()){
-							slot.runJob(job);
-							break;
-						}
-					}
-				}
-			}
+			//System.out.println("Batch system is running......");
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			if(queue.isEmpty()||pool.isEmpty())continue;
+			//System.out.println("executing......");
+		
+			try {
+				for(Job job:queue){				
+					if(job.getStat().equalsIgnoreCase("waiting")){
+						for(Slot slot : pool){
+							//System.out.println(job.getId());
+							if(!slot.isActivity()){
+								//System.out.println(slot.getName());
+								slot.runJob(job);
+								break;
+							}
+						}
+					}
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				continue;
+			}
+
 		}
 	}
 
 	public static boolean addJob(int t){
-		return queue.add(new Job(t));
+		Job job = new Job(t);
+		queue.add(job);	
+		return true;
 	}
 	
 	public static boolean submit(Job job){
+		JTable jt = mf.getTable();
+		int row = 0;
+		for(int i=0;i<jt.getRowCount();i++){
+			if(jt.getValueAt(i, 0)==null||jt.getValueAt(i, 0).toString().equalsIgnoreCase("null")){
+				row = i;
+				break;
+			}
+		}
+		if(row ==0){
+			DefaultTableModel dtm =(DefaultTableModel) jt.getModel();
+			String[] data = new String[]{job.getId(),job.getStat()};
+			dtm.addRow(data);
+		}else{
+			jt.setValueAt(job.getId(), row, 0);
+			jt.setValueAt(job.getStat(), row, 1);
+		}
+		//jt.updateUI();
 		return queue.add(job);
 	}
 	public static boolean rmJob(Job job){
+		JTable jt = mf.getTable();
+		int row = 0;
+		while(true){
+			try {
+				for(int i=1;i<jt.getRowCount();i++){		
+					if(jt.getValueAt(i, 0).toString().equalsIgnoreCase(job.getId())){
+						row = i;
+						break;
+					}	
+				}
+				break;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				continue;
+			}
+		}
+		DefaultTableModel dtm =(DefaultTableModel) jt.getModel();
+		dtm.removeRow(row);
+		
+		//dtm.addRow(new String[10]);
+		//jt.removeRowSelectionInterval(row, row);		
 		return queue.remove(job);
 	}
 	
@@ -86,4 +157,5 @@ public class Sbs {
 	}
 	private static ArrayList<Slot> pool;
 	private static ArrayList<Job> queue;
+	public static MyFrame mf;
 }
